59dbe6b673957471df0dd95d3e2dd3d350fded16
cgood404 <chrisgood404@gmail.com>
2018-04-06 09:59:36 -0400

    Initialize git repo

---
+++ mqttesweb/home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+# Create your views here.
+
+FOLD_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def index(request):
+    return HttpResponse(open(os.path.join(FOLD_DIR, "index.html")).read())
