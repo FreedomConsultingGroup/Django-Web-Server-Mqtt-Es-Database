5babdee31a0bbd4654169031982b153705fe3375
cgood404 <chrisgood404@gmail.com>
2018-04-20 09:05:37 -0400

    allowed host

--- mqttesweb/mqttesweb/settings.py
+++ mqttesweb/mqttesweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = open("/home/ubuntu/keys/djangosecret.key").read().replace('\n', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cgood.fcgit.net"]
 
 
 # Application definition
