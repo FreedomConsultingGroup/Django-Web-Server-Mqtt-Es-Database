7c81f405fc625c770164938c15d4e33d00614d8f
cgood404 <chrisgood404@gmail.com>
2018-04-20 09:56:03 -0400

    Set debug to False

--- mqttesweb/home/views.py
+++ mqttesweb/home/views.py
@@ -9,3 +9,7 @@ FOLD_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def index(request):
     return HttpResponse(open(os.path.join(FOLD_DIR, "statics/index.html")).read())
+
+
+def esGET():
+    pass
